2c6793c0670b5ee7da7247d263bc44650c60508c
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-09-17 23:54:26 +0200

    feat(clock): adding correct logic for the clock time calculation

--- src/components/ClockBase.tsx
+++ src/components/ClockBase.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import {
   Canvas,
   Circle,
   Group,
   Line,
-  useClockValue,
   useComputedValue,
   vec,
 } from '@shopify/react-native-skia';
@@ -13,70 +12,103 @@ import {
 const width = 256;
 const height = 256;
 
+const ONE_SECOND_IN_MS = 1000;
+const ONE_MINUTE_IN_MS = ONE_SECOND_IN_MS * 60;
+const ONE_HOUR_IN_MS = ONE_MINUTE_IN_MS * 60;
+
+const SECOND_HANDLE_SIZE = 0.09;
+const MINUTE_HANDLE_SIZE = 0.09;
+const HOUR_HANDLE_SIZE = 0.5;
+
+const R = width / 2;
+
 function degreesToRadians(degrees: number) {
   var pi = Math.PI;
   return degrees * (pi / 180);
 }
 
 export const ClockBase = () => {
-  const [currentSeconds] = useState(new Date().getSeconds());
-  const clock = useClockValue();
+  const [currentSeconds, setCurrentSeconds] = useState(new Date().getSeconds());
+  const [currentMinutes, setCurrentMinutes] = useState(new Date().getMinutes());
+  const [currentHours, setCurrentHours] = useState(new Date().getHours());
 
   const secondsRotation = useComputedValue(() => {
-    return [{ rotate: degreesToRadians(clock.current / 100) }];
-  }, [clock]);
+    console.log({ currentSeconds });
+    return [{ rotate: degreesToRadians(currentSeconds * 6) }];
+  }, [currentSeconds]);
 
   const minutesRotation = useComputedValue(() => {
-    return [{ rotate: degreesToRadians(clock.current / (60 * 60)) }];
-  }, [clock]);
+    return [{ rotate: degreesToRadians(currentMinutes * 6) }];
+  }, [currentMinutes]);
 
   const hoursRotation = useComputedValue(() => {
-    return [{ rotate: degreesToRadians(clock.current / (60 * 60 * 60)) }];
-  }, [clock]);
+    return [{ rotate: degreesToRadians(currentHours * 30) }];
+  }, [currentHours]);
 
-  const r = width / 2;
+  useEffect(() => {
+    const increaseSeconds = setInterval(() => {
+      setCurrentSeconds((val) => val + 1);
+    }, ONE_SECOND_IN_MS);
+
+    const increaseMinutes = setInterval(() => {
+      setCurrentMinutes((val) => val + 1);
+    }, ONE_MINUTE_IN_MS);
+
+    const increaseHours = setInterval(() => {
+      setCurrentHours((val) => val + 1);
+    }, ONE_HOUR_IN_MS);
+
+    return () => {
+      clearInterval(increaseSeconds);
+      clearInterval(increaseMinutes);
+      clearInterval(increaseHours);
+    };
+  }, [setCurrentSeconds]);
 
   return (
     <>
       <Canvas style={{ width, height }}>
         <Group>
-          <Circle cx={r} cy={r} r={r} color="rgba(211,211,211, 0.2)" />
+          <Circle cx={R} cy={R} r={R} color="rgba(211,211,211, 0.2)" />
 
-          <Group origin={{ x: r, y: r }} transform={secondsRotation}>
+          <Group origin={{ x: R, y: R }} transform={secondsRotation}>
             <Line
-              p1={vec(r, r)}
-              p2={vec(r, 0)}
+              p1={vec(R, R)}
+              p2={vec(R, R * SECOND_HANDLE_SIZE)}
               color="red"
               style="stroke"
-              strokeWidth={4}
+              strokeWidth={2}
             />
           </Group>
 
-          <Group origin={{ x: r, y: r }} transform={minutesRotation}>
+          <Group origin={{ x: R, y: R }} transform={minutesRotation}>
             <Line
-              p1={vec(r, r)}
-              p2={vec(r, r * 0.2)}
-              color="green"
+              p1={vec(R, R)}
+              p2={vec(R, R * MINUTE_HANDLE_SIZE)}
+              color="gray"
               style="stroke"
               strokeWidth={4}
             />
           </Group>
 
-          <Group origin={{ x: r, y: r }} transform={hoursRotation}>
+          <Group origin={{ x: R, y: R }} transform={hoursRotation}>
             <Line
-              p1={vec(r, r)}
-              p2={vec(r, r * 0.5)}
-              color="blue"
+              p1={vec(R, R)}
+              p2={vec(R, R * HOUR_HANDLE_SIZE)}
+              color="gray"
               style="stroke"
               strokeWidth={4}
             />
           </Group>
 
-          <Circle cx={r} cy={r} r={r / 50} color="black" />
+          <Circle cx={R} cy={R} r={R / 50} color="gray" />
         </Group>
       </Canvas>
 
-      <Text>Seconds: {currentSeconds}</Text>
+      <Text>
+        Current Time:{' '}
+        {`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`}
+      </Text>
     </>
   );
 };
