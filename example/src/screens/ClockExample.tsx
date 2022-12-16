a2bfe15c72e16c099c68eaa01dfc1c771cb63002
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-12-17 00:19:56 +0100

    fix: using typescript parser instead of babel to fix linting

--- example/src/screens/ClockExample.tsx
+++ example/src/screens/ClockExample.tsx
@@ -14,12 +14,16 @@ const BackgroundImages = {
     'https://images.wallpaperscraft.com/image/single/bricks_wall_brick_wall_170871_800x1420.jpg',
   Dark: 'https://images.pexels.com/photos/2469122/pexels-photo-2469122.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   Fire: 'https://wallpapercave.com/wp/wp5080466.jpg',
-  Chocolate: 'https://images.unsplash.com/photo-1543233604-3baca4d35513?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29mZmVlJTIwY3VwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
-  Rainbow: 'https://i.pinimg.com/736x/5c/14/d5/5c14d57b1252e2dfa7884bf35caddeb4.jpg',
+  Chocolate:
+    'https://images.unsplash.com/photo-1543233604-3baca4d35513?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29mZmVlJTIwY3VwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+  Rainbow:
+    'https://i.pinimg.com/736x/5c/14/d5/5c14d57b1252e2dfa7884bf35caddeb4.jpg',
   Ice: 'https://i.pinimg.com/736x/df/dc/23/dfdc234070d1f696db057d935daaa1e5.jpg',
   Retro: 'https://wallpapercave.com/wp/wp5679199.jpg',
-  Christmas: 'https://worldofprintables.com/wp-content/uploads/2021/09/Christmas-iPhone-Aesthetic-Wallpaper-Decorations-473x1024.jpg',
-  Halloween: 'https://wallpapercrafter.com/desktop/368169-Holiday-Halloween-Phone-Wallpaper.jpg',
+  Christmas:
+    'https://worldofprintables.com/wp-content/uploads/2021/09/Christmas-iPhone-Aesthetic-Wallpaper-Decorations-473x1024.jpg',
+  Halloween:
+    'https://wallpapercrafter.com/desktop/368169-Holiday-Halloween-Phone-Wallpaper.jpg',
 };
 
 export function ClockExample() {
