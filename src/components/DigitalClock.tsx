5baadd6fc4237f62272cee60a0f1a7eb213fd020
Mateo Guzmán <info@mateoguzman.net>
2023-09-30 12:51:43 +0200

    fix(build): temporarily silent ts issues (#7)

--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -47,12 +47,14 @@ export function DigitalClock({
   const widthForFullNumbers = font.getTextWidth('00:00:00');
 
   return (
+    // @ts-ignore View isn't a valid JSX element...
     <View
       style={[
         styles.container,
         { backgroundColor, width: widthForFullNumbers + PADDING },
       ]}
     >
+      {/* @ts-ignore Canvas isn't a valid JSX element... */}
       <Canvas style={{ width: textWidth, height: fontSize }}>
         <Text
           y={fontSize * 0.8}
